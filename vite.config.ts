b0c10dacd18044022122bc319ffb94b953f96ffa
Wills Stern <willsstern@gmail.com>
2025-01-07 18:25:48 -0800

    trying with different base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: CONFIG.base,
   plugins: [
     react(),
     createHtmlPlugin({
