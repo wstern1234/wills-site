42433c895a32ca690e46e9105005989dd7a0381f
Wills Stern <willsstern@gmail.com>
2025-01-07 19:00:47 -0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/wills-site/sw.js', { scope: '/wills-site/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
