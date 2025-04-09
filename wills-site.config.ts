9a0ebaa3d536ade43faed4840a241e6d087b52a0
Wills Stern <willsstern@gmail.com>
2025-04-09 15:25:32 -0700

    switched logo sources from online to local

--- wills-site.config.ts
+++ wills-site.config.ts
@@ -36,15 +36,14 @@ const CONFIG = {
           description:
             'Chief Technology Officer\nSep 2023 - May 2024\n\nLeading technical projects and affairs in the student government senate for UC Berkeley’s student body.',
           imageUrl:
-            'https://media.licdn.com/dms/image/v2/C4D0BAQFymoWf0GCpkg/company-logo_200_200/company-logo_200_200/0/1631325482780?e=1743033600&v=beta&t=L_lQxF25gjPXvGr6mvz-VAFVJ4xB8fvWlkEaPZhS_Gs',
+            './src/assets/asuc.jpeg',
           link: 'https://studentunion.berkeley.edu/',
         },
         {
           title: 'Lawrence Berkeley National Lab',
           description:
             'Researcher, Programmer\nJun 2021 - Aug 2021\n\nResearched with an LBNL biophysicist to study pentapeptide structures using AI/ML, serialization, and protein data bank APIs to process large amounts of protein structure data.',
-          imageUrl:
-            'https://media.licdn.com/dms/image/v2/C4E0BAQFfc1NOEp9DgA/company-logo_200_200/company-logo_200_200/0/1640018826417/lawrence_berkeley_national_laboratory_logo?e=1743033600&v=beta&t=Vgr0jymbn6urIrR2c8qxGNrg7Z4dKPxw-_IyxGC86ks',
+          imageUrl: './src/assets/lbnl.jpeg',
           link: 'https://www.lbl.gov/',
         },
         {
@@ -52,7 +51,7 @@ const CONFIG = {
           description:
             'Astrophysics Researcher\nJan 2019 - Aug 2019\n\nCreated a rocket experiment, sponsored by NASA, to detect phosphorescence caused by solar radiation in low Earth orbit.',
           imageUrl:
-            'https://media.licdn.com/dms/image/v2/C4D0BAQGRBHWCcaAqGg/company-logo_200_200/company-logo_200_200/0/1630507197379/nasa_logo?e=1743033600&v=beta&t=mOJQvDuGGDFDqq-LFb28oYHMzkbwq8_iwRcRqUtL3nE',
+            './src/assets/nasa.jpeg',
           link: 'https://www.nasa.gov/wallops/',
         },
       ],
